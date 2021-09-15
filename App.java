be47215927238f3b282e5024e8735c11b7792682
Wendeltrs <85528842+Wendeltrs@users.noreply.github.com>
2021-09-15 17:51:55 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner teste = new Scanner(System.in);
+        double notas = 0;
+        double media = 0;
+        double count = 0;
+        double soma = 0;
+        for(int x = 1; x <= 3; x ++){
+            System.out.print("Digite o valor da "+x+"ª nota:\n");
+            notas = teste.nextDouble();
+            soma += notas;
+            count ++;
+        }
+        media = soma/count;
+        System.out.print("A média é: "+media+"\n") ;
+        if(media >= 8 && media <= 10){
+            System.out.print("Conceito A");
+        }else if(media >= 7 && media < 8){
+            System.out.print("Conceito B");
+        }else if(media >= 6 && media < 7){
+            System.out.print("Conceito C");
+        }else if(media >= 5 && media < 6){
+            System.out.print("Conceito D");
+        }else{
+            System.out.print("Conceito E");
+        }      
+        teste.close();
+    }
+}
