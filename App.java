1593704da4180c0cb33500d89c21629979e3ae2c
Wendeltrs <85528842+Wendeltrs@users.noreply.github.com>
2021-09-20 20:08:15 -0300

    Add files via upload
    Programa para calcular a média aritmética e ponderada de 3 notas.

--- App.java
+++ App.java
@@ -1,30 +1,54 @@
+package app;
 import java.util.Scanner;
-public class App {
-    public static void main(String[] args) {
+public class App{
+    public static void main(String[]args){
         Scanner teste = new Scanner(System.in);
-        double notas = 0;
-        double media = 0;
-        double count = 0;
-        double soma = 0;
-        for(int x = 1; x <= 3; x ++){
-            System.out.print("Digite o valor da "+x+"ª nota:\n");
-            notas = teste.nextDouble();
-            soma += notas;
-            count ++;
-        }
-        media = soma/count;
-        System.out.print("A média é: "+media+"\n") ;
-        if(media >= 8 && media <= 10){
-            System.out.print("Conceito A");
-        }else if(media >= 7 && media < 8){
-            System.out.print("Conceito B");
-        }else if(media >= 6 && media < 7){
-            System.out.print("Conceito C");
-        }else if(media >= 5 && media < 6){
-            System.out.print("Conceito D");
-        }else{
-            System.out.print("Conceito E");
-        }      
+
+        Double nota1 = 0.0;
+        Double nota2 = 0.0;
+        Double nota3 = 0.0;
+        String letra = "";
+        Double m = 0.0;
+
+        System.out.println("Digite o valor da nota 1: ");
+        nota1 = teste.nextDouble();
+
+        System.out.println("Digite o valor da nota 2: ");
+        nota2 = teste.nextDouble();
+
+        System.out.println("Digite o valor da nota 3: ");
+        nota3 = teste.nextDouble();
+
+        do{
+
+            System.out.println("Digite uma letra: ");
+            letra = teste.next();
+            System.out.println(letra);
+            m = calcularMedia(nota1,nota2,nota3,letra);
+
+            if(letra.equals("A")){
+                System.out.println("A média aritmética é: "+m);
+                break;
+            }else{
+                System.out.println("A média ponderada é: "+m);
+                break;
+            }
+
+        }while(letra.equals("A") || letra.equals("P"));
+
         teste.close();
     }
-}
+
+    public static Double calcularMedia(double n1, double n2, double n3, String l){      
+        Double media = 0.0;
+
+        if(l.equals("A")){
+            media = (n1 + n2 + n3)/3;
+        }else{
+            media = (n1*5 + n2*3 + n3*2)/10;
+        }
+
+        return media;
+    }
+
+}   
