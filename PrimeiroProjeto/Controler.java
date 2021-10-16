f654e7aaf77f291dad78bc519de7e5e481b09100
Wendel <85528842+Wendeltrs@users.noreply.github.com>
2021-10-16 14:33:13 -0300

    Conexão com Banco de Dados

---
+++ PrimeiroProjeto/Controler.java
@@ -0,0 +1,19 @@
+package PrimeiroProjeto;
+
+public class Controler extends Modelo{
+    public Controler(int id,String nome,String email,String telefone,String senha){
+        super(id, nome, email, telefone, senha);
+    }
+
+    public Controler(){}
+
+
+    public String toString(){
+        return "Id: " + super.getId() + 
+                "\nNome: " + super.getNome() +
+                "\nEmail: " + super.getEmail() +
+                "\nTelefone: " + super.getTelefone() +
+                "\nSenha: " + super.getSenha();
+    }
+    
+}
---
+++ PrimeiroProjeto/Modelo.java
@@ -0,0 +1,58 @@
+package PrimeiroProjeto;
+
+public abstract class Modelo {
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String senha;
+
+
+    public Modelo(int id, String nome, String email, String telefone, String senha){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    public Modelo(){}
+
+
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+
+}
